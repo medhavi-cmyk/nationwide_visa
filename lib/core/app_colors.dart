275b90a148230546029a75648f112ba81268b72c
medhavi <medhavi@nationwidevisas.com>
2026-03-14 17:59:37 +0530

    added colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  // BRAND COLORS
   static const Color primaryRed = Color(0xFFC00A15);
+  static const Color primaryRedDark = Color(0xFF9E0812);
+  static const Color primaryRedLight = Color(0xFFE53935);
   static const Color accentRed = Color(0xFFFF4D4D);
+
+  // BACKGROUND COLORS
+  static const Color backgroundPrimary = Color(0xFFFFFFFF);
+  static const Color backgroundSecondary = Color(0xFFF9FAFB);
   static const Color backgroundRed = Color(0xFFC00A15);
+  static const Color backgroundCard = Color(0xFFFFFFFF);
+
+  // TEXT COLORS
   static const Color textBlack = Color(0xFF1F2937);
+  static const Color textDark = Color(0xFF111827);
   static const Color textGrey = Color(0xFF4B5563);
+  static const Color textLightGrey = Color(0xFF9CA3AF);
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+  // BORDER / DIVIDER
   static const Color borderGrey = Color(0xFFE5E7EB);
+  static const Color dividerGrey = Color(0xFFF3F4F6);
+
+  // ACCENT COLORS
   static const Color statBlue = Color(0xFF4B39EF);
-  static const Color green = Colors.green;
+  static const Color accentBlue = Color(0xFF2563EB);
+  static const Color accentLightBlue = Color(0xFF60A5FA);
+
+  // STATUS COLORS
+  static const Color success = Color(0xFF50CE54);
+  static const Color warning = Color(0xFFF59E0B);
+  static const Color error = Color(0xFFEF4444);
+  static const Color info = Color(0xFF3B82F6);
 }
